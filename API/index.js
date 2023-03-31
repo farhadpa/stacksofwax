1303ad67cc78523b3957e430811a1c0f8f678716
Farhad Panahi <fpanahi01@qub.ac.uk>
2023-03-31 14:39:19 +0100

    Database connection implemented with .env configs. added sample database call and query to '/' route. dotenv package added to dependencies.

--- API/index.js
+++ API/index.js
@@ -1,9 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 const homeRoute = require('./routes/home');
+
 
 app.use('/', homeRoute);
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log('Server is running');
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 10,
+    port: process.env.DB_PORT,
+    multipleStatements: true
+});
+
+app.set('db', connection);
+
+connection.getConnection((err) => {
+    if (err) return console.log(err.message);
+    console.log("Connected to the MySQL server.");
+});
+
+app.listen((process.env.PORT || 4004), () => {
+  console.log(`Server is running on ${process.env.PORT || 4004}`);
 });
--- API/routes/home.js
+++ API/routes/home.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+
 router.get("/", (req, res) => {
     try {
-        res.json("Hello World");
+        let sql = "SELECT * FROM collection";
+        req.app.get('db').query(sql, (err, result) => {
+            if (err) return console.log(err.message);
+            res.json(result);
+        });
     } catch (err) {
         console.error(err.message);
     }   
